821913165b3886df9c064cb1b066454a10a0e7ba
chobopractice <79438586+chobopractice@users.noreply.github.com>
2022-02-12 19:30:43 +0900

    Add files via upload

---
+++ build/precache-manifest.98ab4e8296ea3a8384090333ef8ecdc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131426c6bbe62500c5a82e07dcf2a786",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daa76bee99c8bf3fd64f",
+    "url": "/static/css/2.79572187.chunk.css"
+  },
+  {
+    "revision": "5abf644e0a806d7a0f0d",
+    "url": "/static/css/main.456b093b.chunk.css"
+  },
+  {
+    "revision": "daa76bee99c8bf3fd64f",
+    "url": "/static/js/2.191415e5.chunk.js"
+  },
+  {
+    "revision": "58d56cc2c167f8eb4c6d41e7d9bc297d",
+    "url": "/static/js/2.191415e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abf644e0a806d7a0f0d",
+    "url": "/static/js/main.02784fa8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
